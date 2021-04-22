64efc977b61741113113663199b442ce2a001517
erlandego <50873837+erlandego@users.noreply.github.com>
2021-04-22 17:53:45 +0800

    abc
    aku ngubah nama db nya

--- foodfun-master/conn.php
+++ foodfun-master/conn.php
@@ -7,7 +7,10 @@ $dbname = "review_makanan";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if($conn->connect_error)
-	{
-	die("connection failed: " . $conn->connect_error);
-	}
+{
+		die("connection failed: " . $conn->connect_error);
+}
+else{
+	echo "sukses";
+}
 ?>
