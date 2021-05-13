94e4507637cbde2fdb6d002a10d6149329e37140
stefanie <stefanieangelina.sa@gmail.com>
2021-05-13 12:05:55 +0700

    indestasi

--- foodfun-master/submitUlasan.php
+++ foodfun-master/submitUlasan.php
@@ -1,16 +1,19 @@
 <?php 
-session_start();
-    require_once("conn.php");
-    $idRestoran = $_POST['id_restoran'];
-    $ulasan = $_POST['ulasan'];
-    $idUser = $_SESSION['idLogin'];
-    $queryinsert = "INSERT into komentar values('', '$idUser', '$idRestoran', '$ulasan')";
-    $result = mysqli_query($conn, $queryinsert);
-    if ($result) {
-      header('location: /foodfun-master/home.php');    
-    }else {
-        echo mysqli_error($conn);
-        die();
-    }
-
+  session_start();
+  require_once("conn.php");
     
+  $idRestoran = $_POST['id_restoran'];
+  $ulasan = $_POST['ulasan'];
+  $idUser = $_SESSION['idLogin'];
+
+  $queryinsert = "INSERT into komentar values('', '$idUser', '$idRestoran', '$ulasan')";
+  $result = mysqli_query($conn, $queryinsert);
+  
+  if ($result) {
+    header('location: /foodfun-master/home.php');    
+  }else {
+      echo mysqli_error($conn);
+      die();
+  }
+
+?>
