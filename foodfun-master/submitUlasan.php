709932b00997370cebadea9001153c0cbac5cd37
adellaanwary55@gmail.com <adellaanwary55@gmail.com>
2021-05-26 16:07:51 +0700

    Make submit comment star for rating and images on detail restaurant

--- foodfun-master/submitUlasan.php
+++ foodfun-master/submitUlasan.php
@@ -8,12 +8,20 @@
     
   $idRestoran = $_POST['id_restoran'];
   $ulasan = $_POST['ulasan'];
+  $rating = $_POST['rating'];
   $idUser = $_SESSION['idLogin'];
 
-  $queryinsert = "INSERT into komentar values('', '$idUser', '$idRestoran', '$ulasan')";
+  $queryinsert = "INSERT into komentar values('', '$idUser', '$idRestoran', '$ulasan',$rating)";
   $result = mysqli_query($conn, $queryinsert);
   
   if ($result) {
+    $selectAvg = "SELECT AVG(rating) as rating from komentar where id_restoran=$idRestoran";
+    $resultAvg = mysqli_query($conn, $selectAvg)->fetch_all(MYSQLI_ASSOC);
+    if (!empty($resultAvg)) {
+      $avg = $resultAvg[0]['rating'];
+      $queryupdate = "UPDATE restoran set rating=$avg where id_restoran=$idRestoran";
+      mysqli_query($conn, $queryupdate);
+    }
     header('location: /foodfun-master/restoranDetail.php?resto='. $idRestoran);    
   }else {
       echo mysqli_error($conn);
